49d5f4debe0a46fdc505015bc9fa85d3f50ba692
Marek Przechrzta <prtm@viessmann.com>
2025-10-07 19:10:05 +0200

    Refactor signin route for improved readability and error handling

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,13 +1,13 @@
-import { adminClient, client } from "@/lib/appwrite/client";
-import { NextRequest, NextResponse } from "next/server";
-import { Account, Teams } from "node-appwrite";
-import { z } from "zod";
+import { adminClient, client } from '@/lib/appwrite/client';
+import { NextRequest, NextResponse } from 'next/server';
+import { Account, Teams } from 'node-appwrite';
+import { z } from 'zod';
 
 const FormSchema = z.object({
-  email: z.email({ message: "Invalid email address" }),
-  password: z
-    .string()
-    .min(8, { message: "Password must have at least 8 characters" }),
+  email: z.email({ message: 'Invalid email address' }),
+  password: z.string().min(8, {
+    message: 'Password must have at least 8 characters',
+  }),
   domain: z.string().optional(),
 });
 
@@ -16,45 +16,50 @@ export async function POST(request: NextRequest) {
 
   const { success, error } = FormSchema.safeParse(payload);
   if (!success) {
-    return NextResponse.json({ errors: error.issues }, { status: 422 });
-  }
-
-  if (payload.domain) {
-    const teams = new Teams(adminClient);
-    const team = await teams.get({ teamId: payload.domain });
-    if (!team) {
-      return NextResponse.json(
-        {
-          error: `The app ${payload.domain} does not exist in IAM`,
-          errors: [],
-        },
-        { status: 404 },
-      );
-    }
-
-    const memberships = await teams.listMemberships({ teamId: payload.domain });
-    const isUserMember = memberships.memberships.find(
-      (m) => m.userEmail === payload.email,
+    return NextResponse.json(
+      { errors: error.issues },
+      { status: 422 },
     );
-    if (!isUserMember) {
-      return NextResponse.json(
-        {
-          error: `You have no access to ${payload.domain} app`,
-          errors: [],
-        },
-        { status: 404 },
-      );
-    }
   }
-
-  const account = new Account(adminClient);
-
-  //find team for domain if not found throw 404 App not found
-  //check if user belogns to team
-  //if not throw 403
 
   try {
-    const session = await account.createEmailPasswordSession(payload);
+    const account = new Account(adminClient);
+    const session =
+      await account.createEmailPasswordSession(payload);
+
+    if (payload.domain) {
+      const teams = new Teams(adminClient);
+      const team = await teams.get({
+        teamId: payload.domain,
+      });
+      if (!team) {
+        return NextResponse.json(
+          {
+            error: `The app ${payload.domain} does not exist in IAM`,
+            errors: [],
+          },
+          { status: 404 },
+        );
+      }
+
+      const memberships = await teams.listMemberships({
+        teamId: payload.domain,
+      });
+      const isUserMember = memberships.memberships.find(
+        (m) => m.userEmail === payload.email,
+      );
+
+      if (!isUserMember) {
+        await teams.createMembership({
+          teamId: payload.domain,
+          roles: ['member'],
+          userId: session.userId,
+          email: payload.email,
+          name: payload.name,
+        });
+      }
+    }
+
     return NextResponse.json(
       {
         error: null,
